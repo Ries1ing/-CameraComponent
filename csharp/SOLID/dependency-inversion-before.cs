29d2f29f08b48d58f1350540f6d0f1ce78c2e052
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2021-08-15 22:00:43 +0000

    l10n: Updates to Danish (da) translation

--- csharp/SOLID/dependency-inversion-before.cs
+++ csharp/SOLID/dependency-inversion-before.cs
@@ -1,3 +1,4 @@
 //  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/dependency-inversion-before.py
 
-namespace DependencyInversi
+namespace DependencyInversionBefore {
+    public
