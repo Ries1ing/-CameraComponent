8583b9452570ccfa2bf0d2898007eadf098748d4
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2021-08-31 08:04:06 +0000

    media: mantis: fix silly crash case

--- csharp/SOLID/dependency-inversion-before.cs
+++ csharp/SOLID/dependency-inversion-before.cs
@@ -10,4 +10,4 @@ namespace DependencyInversionBefore {
         public Order() {
             this.Items = new List<string>();
             this.Quantities = new List<int>();
-       
+            this.Prices = new List<double>(
