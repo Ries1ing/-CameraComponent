bdfa0db3494212acea2a7d80288da8177dfa1f0a
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2021-09-07 06:40:25 +0000

    Adding contributors

--- csharp/SOLID/dependency-inversion-before.cs
+++ csharp/SOLID/dependency-inversion-before.cs
@@ -17,4 +17,7 @@ namespace DependencyInversionBefore {
         public void AddItem(string name, int quantity, double price) {
             this.Items.Add(name);
             this.Quantities.Add(quantity);
-            this.Prices.Add(p
+            this.Prices.Add(price);
+        }
+
+  
