5b0505a21f1f712dc851959ecf0de6c9bc56cded
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2021-09-09 21:06:44 +0000

    sound

--- csharp/SOLID/dependency-inversion-before.cs
+++ csharp/SOLID/dependency-inversion-before.cs
@@ -25,4 +25,6 @@ namespace DependencyInversionBefore {
 
             for (int i = 0; i < this.Prices.Count; i++) {
                 total += this.Quantities[i] * this.Prices[i];
- 
+            }
+
+            r
