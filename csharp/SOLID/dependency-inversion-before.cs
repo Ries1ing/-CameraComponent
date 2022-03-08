0fc95f3e31fe56e433862cd0c51dca93327fcfb3
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-03-08 15:03:08 +0000

    out

--- csharp/SOLID/dependency-inversion-before.cs
+++ csharp/SOLID/dependency-inversion-before.cs
@@ -113,4 +113,4 @@ namespace DependencyInversionBefore {
             Console.WriteLine(order.TotalPrice());
             SMSAuthorizer authorizer =  new SMSAuthorizer();
             authorizer.VerifyCode("465839");
-            IPaymentProcessor processor = new DebitP
+            IPaymentProcessor processor = new DebitPaymentProcessor("2349875"
