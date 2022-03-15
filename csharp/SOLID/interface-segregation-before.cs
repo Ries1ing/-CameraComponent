a09b8dc50e3bacbee6921f79fc42a557e6c70dcd
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-03-15 09:51:15 +0000

    correct README.md

--- csharp/SOLID/interface-segregation-before.cs
+++ csharp/SOLID/interface-segregation-before.cs
@@ -13,3 +13,23 @@ namespace InterfaceSegregationBefore {
             this.Quantities = new List<int>();
             this.Prices = new List<double>();
             this.Status = "open";
+        }
+
+        public void AddItem(string name, int quantity, double price) {
+            this.Items.Add(name);
+            this.Quantities.Add(quantity);
+            this.Prices.Add(price);
+        }
+
+        public double TotalPrice() {
+            double total = 0;
+
+            for (int i = 0; i < this.Prices.Count; i++) {
+                total += this.Quantities[i] * this.Prices[i];
+            }
+
+            return total;
+        }
+    }
+
+    public interface IPaymentProcessor {
