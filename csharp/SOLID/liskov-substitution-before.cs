606cf1ebc404332f533f34c7b10f47d063badaa2
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-04-01 16:29:39 +0000

    - minor fix

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -1,1 +1,3 @@
-//  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-be
+//  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-before.py
+
+namespace Lis
