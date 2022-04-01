4f7557ee59c13257538fd59812fa30e9dce3afd3
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-04-01 22:56:14 +0000

    Text change.

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -1,3 +1,3 @@
 //  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-before.py
 
-namespace Lis
+namespace LiskovSubstitutionBefore {
