64735450a271d52f6187cb11db52e8d3c54300ef
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-04-02 06:41:15 +0000

    Add splash screen

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -1,3 +1,4 @@
 //  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-before.py
 
 namespace LiskovSubstitutionBefore {
+    public class Orde
