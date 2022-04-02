ab1338a9806bb748d12712e76f07761e8598597d
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-04-02 11:54:20 +0000

    chore(release): 0.0.2

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -1,4 +1,5 @@
 //  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-before.py
 
 namespace LiskovSubstitutionBefore {
-    public class Orde
+    public class Order {
+        public List<strin
