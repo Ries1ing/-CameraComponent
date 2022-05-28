6e0ef4f7073a26626db8ff1b8d9f9a16cd6af308
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-05-28 18:10:14 +0000

    - converted neededforbuild to BuildRequires

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -17,4 +17,4 @@ namespace LiskovSubstitutionBefore {
         public void AddItem(string name, int quantity, double price) {
             this.Items.Add(name);
             this.Quantities.Add(quantity);
-            thi
+            this.Prices.Add(price);
