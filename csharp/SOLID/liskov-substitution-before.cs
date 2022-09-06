ec1d83c0f9e12cfab200b6148c164dde36a70a45
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-09-06 07:37:01 +0000

    update tc

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -32,4 +32,4 @@ namespace LiskovSubstitutionBefore {
     }
 
     public interface IPaymentProcessor {
-        public void Pay(Order order,
+        public void Pay(Order order, string securityCode
