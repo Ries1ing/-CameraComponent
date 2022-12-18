261766e92dec637d6b5ca1e88056c450cab10d82
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-12-18 06:35:58 +0000

    ARM: LPAE: use signed arithmetic for mask definitions

--- csharp/SOLID/open-closed-before.cs
+++ csharp/SOLID/open-closed-before.cs
@@ -7,3 +7,31 @@ namespace OpenClosedBefore {
         public List<int> Quantities { get; set; }
         public List<double> Prices { get; set; }
         public string Status { get; set; }
+
+        public Order() {
+            this.Items = new List<string>();
+            this.Quantities = new List<int>();
+            this.Prices = new List<double>();
+            this.Status = "open";
+        }
+
+        public void AddItem(string name, int quantity, double price) {
+            this.Items.Add(name);
+            this.Quantities.Add(quantity);
+            this.Prices.Add(price);
+        }
+
+        public double TotalPrice() {
+            double total = 0;
+
+            for (int i = 0; i < this.Prices.Count; i++) {
+                total += this.Quantities[i] * this.Prices[i];
+            }
+
+            return total;
+        }
+    }
+
+    public class PaymentProcessor {
+        public static void PayDebit(Order order, string securityCode) {
+            Console.WriteLine("Processing debit payment type");
