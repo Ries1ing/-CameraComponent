efa186e0338a4e670e56e545d7f0f6336afb64f9
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-12-20 09:01:16 +0000

    Added unittest

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -1,1 +1,1 @@
-// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%2
+// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-respons
