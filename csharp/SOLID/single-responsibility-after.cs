ea06369bc1014ac7d1fd1a16d47d7a9653012231
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-12-20 20:14:19 +0000

    WIP [ci skip]

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -1,1 +1,3 @@
-// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-respons
+// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-after.py
+
+namespace
