fa91bd9c145e28f17b19d98b6a102cf705d729fd
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2022-12-21 16:34:36 +0000

    Minor spelling fix

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -1,4 +1,5 @@
 // This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-after.py
 
 namespace SingleResponsibilityAfter {
-    public cla
+    public class Order {
+        public
