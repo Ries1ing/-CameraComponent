5dd788b42c687ad75afe84160b7758060eafcb93
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-01-04 13:20:01 +0000

    Create css.css

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -23,4 +23,4 @@ namespace SingleResponsibilityAfter {
         public double TotalPrice() {
             double total = 0;
 
-            for (int i = 0; i < this
+            for (int i = 0; i < this.Prices.Count; i
