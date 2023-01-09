da146a0816033627e01582cafb63dde2b0b48693
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-01-09 15:32:05 +0000

    Comments updated

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -34,4 +34,4 @@ namespace SingleResponsibilityAfter {
     public class PaymentProcessor {
         public static void PayDebit(Order order, string securityCode) {
             Console.WriteLine("Processing debit payment type");
-            Console.WriteLine($"Verifying secu
+            Console.WriteLine($"Verifying security code: {securityCo
