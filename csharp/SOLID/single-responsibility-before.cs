2e5bec9ac9a5514b4e528f51cfc032638b8cb8a6
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-02-03 07:05:04 +0000

    strechable skin

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -1,2 +1,2 @@
 // This file is derived from:
-// https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/
+// https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-b
