a051c9fa682d5aa430ba9d14c634d9addbecbfdf
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-02-03 18:48:26 +0000

    Updated Resume

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -1,3 +1,3 @@
 // This file is derived from:
 // https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-before.py
-// This is meant to be 
+// This is meant to be a bad example that breaks 
