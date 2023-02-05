d7f5b6d2a1f987abb6bd5a8dc0ab1c873d98afc0
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-02-05 17:45:16 +0000

    Fixed a minor bug.

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -1,3 +1,5 @@
 // This file is derived from:
 // https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-before.py
-// This is meant to be a bad example that breaks the single-responsibility principl
+// This is meant to be a bad example that breaks the single-responsibility principle.
+
+namespace SingleRespon
