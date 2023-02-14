b29bf96d435a910715ee3b20b3a3e9c4c189a894
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-02-14 12:09:03 +0000

    Use latest orbit-db-store

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -16,4 +16,4 @@ namespace SingleResponsibilityBefore {
             this.Status = "open";
         }
 
-        public void AddItem(string name, int
+        public void AddItem(string name, int quantity, double pri
