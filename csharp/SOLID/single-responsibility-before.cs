b9dd73bdc674047867be08c1d8506d828f058822
Ries1ing <125125817+Ries1ing@users.noreply.github.com>
2023-03-25 09:55:58 +0000

    Bump dependencies.

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -41,4 +41,6 @@ namespace SingleResponsibilityBefore {
             else if (paymentType == "credit") {
                 Console.WriteLine("Processing credit payment type");
                 Console.WriteLine($"Verifying security code: {securityCode}");
-                this.Status = "p
+                this.Status = "paid";
+            }
+ 
